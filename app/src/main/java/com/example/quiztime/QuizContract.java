4d2fc31778deebb665ce1e94092acab1ad7eb30b
Kyle Lemons <klemons15@hotmail.com>
2019-10-16 08:44:13 -0500

    added Question class with constructors, getters/setters for attributes

---
+++ app/src/main/java/com/example/quiztime/QuizContract.java
@@ -0,0 +1,4 @@
+package com.example.quiztime;
+
+public final class QuizContract {
+}
--- app/src/main/java/com/example/quiztime/StartingPageActivity.java
+++ app/src/main/java/com/example/quiztime/StartingPageActivity.java
@@ -1,4 +1,30 @@
 package com.example.quiztime;
 
-public class StartingPageActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startingpage);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz() {
+        Intent intent = new Intent(StartingPageActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
